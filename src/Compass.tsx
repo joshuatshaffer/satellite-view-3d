5c74d9392942eabce6aa3d7404a2f4f5ce7c7c61
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-07-20 14:24:37 -0500

    Figure out directions in scene space.

--- src/Compass.tsx
+++ src/Compass.tsx
@@ -2,6 +2,13 @@ import { useEffect, useState } from "react";
 import * as THREE from "three";
 import { degToRad, deviceOrientationToEuler } from "./rotations";
 
+const north = () => new THREE.Vector3(0, 0, -1);
+const east = () => new THREE.Vector3(1, 0, 0);
+const south = () => new THREE.Vector3(0, 0, 1);
+const west = () => new THREE.Vector3(-1, 0, 0);
+const up = () => new THREE.Vector3(0, 1, 0);
+const down = () => new THREE.Vector3(0, -1, 0);
+
 export function Compass() {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
 
@@ -33,6 +40,25 @@ export function Compass() {
     ]);
     scene.background = texture;
 
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(
+        new Float32Array([
+          ...north().multiplyScalar(100).toArray(),
+          ...east().multiplyScalar(100).toArray(),
+          ...down().multiplyScalar(100).toArray(),
+        ]),
+        3
+      )
+    );
+
+    const particles = new THREE.Points(
+      geometry,
+      new THREE.PointsMaterial({ size: 2, color: 16777215 })
+    );
+    scene.add(particles);
+
     let orientation: DeviceOrientationEvent | null = null;
 
     window.addEventListener(
@@ -47,7 +73,7 @@ export function Compass() {
         camera.quaternion.setFromEuler(deviceOrientationToEuler(orientation));
         camera.quaternion.premultiply(
           new THREE.Quaternion().setFromEuler(
-            new THREE.Euler(degToRad(90), degToRad(180), 0, "XYZ")
+            new THREE.Euler(degToRad(-90), 0, 0, "XYZ")
           )
         );
       }
