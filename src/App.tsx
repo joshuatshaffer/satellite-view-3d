5e1a2ca2fd9a35432f69c492ee3ebe5bb12de449
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-07-20 11:10:04 -0500

    Simplify.

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,8 @@
-import * as satellite from "satellite.js";
-import { CameraPassthrough } from "./CameraPassthrough";
-import { DisplayGeolocationCoordinates } from "./DisplayGeolocationCoordinates";
 import { Orientation } from "./Orientation";
-
-// Sample TLE
-const tleLine1 =
-  "1 25544U 98067A   24200.15235088  .00018477  00000+0  33066-3 0  9997";
-const tleLine2 =
-  "2 25544  51.6371 161.9379 0010265  78.7950 281.4192 15.49981173463357";
-
-// Initialize a satellite record
-const satrec = satellite.twoline2satrec(tleLine1, tleLine2);
-
-//  Or you can use a JavaScript Date
-const positionAndVelocity = satellite.propagate(satrec, new Date());
-
-// The position_velocity result is a key-value pair of ECI coordinates.
-// These are the base results from which all other coordinates are derived.
-const positionEci = positionAndVelocity.position;
-if (typeof positionEci !== "object") {
-  throw new Error("Position is not an object.");
-}
-
-// Set the Observer at 122.03 West by 36.96 North, in RADIANS
-const observerGd = {
-  latitude: satellite.degreesToRadians(51.47783),
-  longitude: satellite.degreesToRadians(-0.00139),
-  height: 0.1420368,
-};
-
-// You will need GMST for some of the coordinate transforms.
-// http://en.wikipedia.org/wiki/Sidereal_time#Definition
-const gmst = satellite.gstime(new Date());
-
-// You can get ECF, Geodetic, Look Angles, and Doppler Factor.
-const positionEcf = satellite.eciToEcf(positionEci, gmst);
-const positionGd = satellite.eciToGeodetic(positionEci, gmst);
-const lookAngles = satellite.ecfToLookAngles(observerGd, positionEcf);
 
 export function App() {
   return (
     <>
-      {JSON.stringify({
-        azimuth: lookAngles.azimuth * (180 / Math.PI),
-        elevation: lookAngles.elevation * (180 / Math.PI),
-        latitudeDeg: satellite.degreesLat(positionGd.latitude),
-        longitudeDeg: satellite.degreesLong(positionGd.longitude),
-        height: positionGd.height,
-      })}
-      <CameraPassthrough />
-      <DisplayGeolocationCoordinates />
       <Orientation />
     </>
   );
--- src/Orientation.tsx
+++ src/Orientation.tsx
@@ -1,17 +1,5 @@
 import { useEffect, useState } from "react";
 
-function useOrientation() {
-  const [orientation, setOrientation] = useState<DeviceOrientationEvent | null>(
-    null
-  );
-  useEffect(() => {
-    const handler = (event: DeviceOrientationEvent) => setOrientation(event);
-    window.addEventListener("deviceorientation", handler);
-    return () => window.removeEventListener("deviceorientation", handler);
-  }, []);
-  return orientation;
-}
-
 function useOrientationAbsolute() {
   const [orientation, setOrientation] = useState<DeviceOrientationEvent | null>(
     null
@@ -25,32 +13,12 @@ function useOrientationAbsolute() {
   return orientation;
 }
 
-function useMotion() {
-  const [motion, setMotion] = useState<DeviceMotionEvent | null>(null);
-  useEffect(() => {
-    const handler = (event: DeviceMotionEvent) => setMotion(event);
-    window.addEventListener("devicemotion", handler);
-    return () => window.removeEventListener("devicemotion", handler);
-  }, []);
-  return motion;
-}
-
 export function Orientation() {
-  const orientation = useOrientation();
   const orientationAbsolute = useOrientationAbsolute();
-  const motion = useMotion();
   return (
     <pre>
       {JSON.stringify(
         {
-          orientation: orientation
-            ? {
-                absolute: orientation.absolute,
-                alpha: orientation.alpha,
-                beta: orientation.beta,
-                gamma: orientation.gamma,
-              }
-            : orientation,
           orientationAbsolute: orientationAbsolute
             ? {
                 absolute: orientationAbsolute.absolute,
@@ -59,33 +27,6 @@ export function Orientation() {
                 gamma: orientationAbsolute.gamma,
               }
             : orientationAbsolute,
-          motion: motion
-            ? {
-                acceleration: motion.acceleration
-                  ? {
-                      x: motion.acceleration.x,
-                      y: motion.acceleration.y,
-                      z: motion.acceleration.z,
-                    }
-                  : motion.acceleration,
-                accelerationIncludingGravity:
-                  motion.accelerationIncludingGravity
-                    ? {
-                        x: motion.accelerationIncludingGravity.x,
-                        y: motion.accelerationIncludingGravity.y,
-                        z: motion.accelerationIncludingGravity.z,
-                      }
-                    : motion.accelerationIncludingGravity,
-                interval: motion.interval,
-                rotationRate: motion.rotationRate
-                  ? {
-                      alpha: motion.rotationRate.alpha,
-                      beta: motion.rotationRate.beta,
-                      gamma: motion.rotationRate.gamma,
-                    }
-                  : motion.rotationRate,
-              }
-            : motion,
         },
         null,
         2
