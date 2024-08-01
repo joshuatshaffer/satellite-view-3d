0f57c77222f023c5c802d2426e1032104275f17c
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-07-31 20:04:31 -0500

    Simplify satellite prediction code.

--- src/satellites.ts
+++ src/satellites.ts
@@ -1,51 +1,115 @@
-import { Scene } from "three";
-import { Satellite } from "./Satellite";
+import * as satellite from "satellite.js";
+import {
+  BufferAttribute,
+  BufferGeometry,
+  Euler,
+  Points,
+  PointsMaterial,
+  Scene,
+} from "three";
+import { observerGd } from "./observer";
+import { north } from "./sceneSpaceDirections";
+
+type Tle = [line1: string, line2: string];
+
+interface SatelliteDefinition {
+  displayName: string;
+  tle: Tle;
+}
 
 export function makeSatellites(scene: Scene) {
-  const satellites = [
-    new Satellite(
-      "ISS",
-      "1 25544U 98067A   24200.15235088  .00018477  00000+0  33066-3 0  9997",
-      "2 25544  51.6371 161.9379 0010265  78.7950 281.4192 15.49981173463357"
-    ),
-    new Satellite(
-      "NOAA 15",
-      "1 25338U 98030A   24205.56694301  .00000578  00000-0  25704-3 0  9999",
-      "2 25338  98.5672 231.9320 0009648 212.5949 147.4637 14.26637206362379"
-    ),
-    new Satellite(
-      "NOAA 18",
-      "1 28654U 05018A   24205.45690124  .00000515  00000-0  29850-3 0  9991",
-      "2 28654  98.8737 282.1203 0013415 323.8830  36.1435 14.13261241988291"
-    ),
-    new Satellite(
-      "NOAA 19",
-      "1 33591U 09005A   24205.55510556  .00000493  00000-0  28834-3 0  9994",
-      "2 33591  99.0451 261.7357 0013813 185.5094 174.5927 14.13055684796509"
-    ),
+  let definitions: SatelliteDefinition[] = [
+    {
+      displayName: "ISS",
+      tle: [
+        "1 25544U 98067A   24200.15235088  .00018477  00000+0  33066-3 0  9997",
+        "2 25544  51.6371 161.9379 0010265  78.7950 281.4192 15.49981173463357",
+      ],
+    },
+    {
+      displayName: "NOAA 15",
+      tle: [
+        "1 25338U 98030A   24205.56694301  .00000578  00000-0  25704-3 0  9999",
+        "2 25338  98.5672 231.9320 0009648 212.5949 147.4637 14.26637206362379",
+      ],
+    },
+    {
+      displayName: "NOAA 18",
+      tle: [
+        "1 28654U 05018A   24205.45690124  .00000515  00000-0  29850-3 0  9991",
+        "2 28654  98.8737 282.1203 0013415 323.8830  36.1435 14.13261241988291",
+      ],
+    },
+    {
+      displayName: "NOAA 19",
+      tle: [
+        "1 33591U 09005A   24205.55510556  .00000493  00000-0  28834-3 0  9994",
+        "2 33591  99.0451 261.7357 0013813 185.5094 174.5927 14.13055684796509",
+      ],
+    },
   ];
 
-  for (const sat of satellites) {
-    scene.add(sat.object3D);
-    if (sat.nextPassLine) {
-      scene.add(sat.nextPassLine);
+  let records = definitions.map((d) =>
+    satellite.twoline2satrec(d.tle[0], d.tle[1])
+  );
+
+  let indexMap: number[] = [];
+
+  let scenePositions = new Float32Array(3 * records.length);
+
+  const geometry = new BufferGeometry();
+  geometry.setAttribute("position", new BufferAttribute(scenePositions, 3));
+  geometry.setDrawRange(0, indexMap.length);
+
+  const particles = new Points(
+    geometry,
+    new PointsMaterial({ size: 5, color: 0xff0000, sizeAttenuation: false })
+  );
+  scene.add(particles);
+
+  const update = (nowDate = new Date()) => {
+    const nowGmst = satellite.gstime(nowDate);
+
+    indexMap = [];
+    for (let i = 0; i < records.length; i++) {
+      const positionAndVelocity = satellite.propagate(records[i], nowDate);
+
+      const positionEci = positionAndVelocity.position;
+
+      if (typeof positionEci !== "object") {
+        continue;
+      }
+
+      const positionEcf = satellite.eciToEcf(positionEci, nowGmst);
+
+      const lookAngles = satellite.ecfToLookAngles(observerGd, positionEcf);
+
+      scenePositions.set(
+        north()
+          .applyEuler(lookAnglesToEuler(lookAngles))
+          .multiplyScalar(100)
+          .toArray(),
+        indexMap.length * 3
+      );
+
+      indexMap.push(i);
     }
-  }
+
+    geometry.setDrawRange(0, indexMap.length);
+    geometry.attributes.position.needsUpdate = true;
+  };
 
   return {
-    update: () => {
-      for (const satellite of satellites) {
-        satellite.update();
-      }
-    },
+    update,
 
     dispose: () => {
-      for (const sat of satellites) {
-        scene.remove(sat.object3D);
-        if (sat.nextPassLine) {
-          scene.remove(sat.nextPassLine);
-        }
-      }
+      scene.remove(particles);
+      particles.geometry.dispose();
+      particles.material.dispose();
     },
   };
+}
+
+function lookAnglesToEuler(lookAngles: satellite.LookAngles) {
+  return new Euler(lookAngles.elevation, -lookAngles.azimuth, 0, "YXZ");
 }
