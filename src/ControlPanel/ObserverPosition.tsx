9ac48ab6ed2bd0032d3abb3726089a5b7bb56ea1
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-08-01 08:13:15 -0500

    Add manual observer position.

--- src/ControlPanel/ObserverPosition.tsx
+++ src/ControlPanel/ObserverPosition.tsx
@@ -1,23 +1,77 @@
-import { useAtomValue } from "jotai";
+import { useAtom } from "jotai";
 import { degToDms, formatAngleUnits } from "../AngleUnits";
-import { observerPositionAtom } from "../settings";
+import { isElementOf } from "../isElementOf";
+import {
+  observerPositionAtom,
+  observerPositionModeAtom,
+  observerPositionModes,
+} from "../settings";
 
 export function ObserverPosition() {
-  const observerPosition = useAtomValue(observerPositionAtom);
+  const [mode, setMode] = useAtom(observerPositionModeAtom);
+  const [{ latitude, longitude }, setPosition] = useAtom(observerPositionAtom);
 
   return (
     <fieldset>
       <legend>Observer Position</legend>
       <div>
+        <label>Mode </label>
+        <select
+          value={mode}
+          onChange={(event) => {
+            const newValue = event.target.value;
+
+            if (!isElementOf(observerPositionModes, newValue)) {
+              throw new Error("Invalid observer position mode");
+            }
+
+            setMode(newValue);
+          }}
+        >
+          <option value="currentPosition">Current position</option>
+          <option value="manual">Manual</option>
+        </select>
+      </div>
+      <div>
         <label>Lat </label>
-        <input type="text" readOnly value={observerPosition.latitude} />
+        <input
+          type="number"
+          min={-90}
+          max={90}
+          value={latitude}
+          onChange={(event) => {
+            const newLatitude = parseFloat(event.target.value);
+
+            if (!isNaN(newLatitude)) {
+              setPosition((prev) => ({
+                ...prev,
+                latitude: newLatitude,
+              }));
+            }
+          }}
+        />
       </div>
       <div>
         <label>Lon </label>
-        <input type="text" readOnly value={observerPosition.longitude} />
+        <input
+          type="number"
+          min={-180}
+          max={180}
+          value={longitude}
+          onChange={(event) => {
+            const newLongitude = parseFloat(event.target.value);
+
+            if (!isNaN(newLongitude)) {
+              setPosition((prev) => ({
+                ...prev,
+                longitude: newLongitude,
+              }));
+            }
+          }}
+        />
       </div>
-      <div>{formatAngleUnits(degToDms(observerPosition.latitude))}</div>
-      <div>{formatAngleUnits(degToDms(observerPosition.longitude))}</div>
+      <div>{formatAngleUnits(degToDms(latitude))}</div>
+      <div>{formatAngleUnits(degToDms(longitude))}</div>
     </fieldset>
   );
 }
--- src/settings.ts
+++ src/settings.ts
@@ -24,7 +24,15 @@ export const viewControlSettingAtom = atomWithStorage<ViewControlSetting>(
   "manual"
 );
 
-export const observerPositionAtom = atomWithStorage<{
+export const observerPositionModes = ["currentPosition", "manual"] as const;
+export type ObserverPositionMode = (typeof observerPositionModes)[number];
+
+export const observerPositionModeAtom = atomWithStorage<ObserverPositionMode>(
+  localStoragePrefix + "observer-position-mode",
+  "currentPosition"
+);
+
+const observerPositionSavedAtom = atomWithStorage<{
   latitude: number;
   longitude: number;
   height: number;
@@ -34,26 +42,64 @@ export const observerPositionAtom = atomWithStorage<{
   height: 0,
 });
 
-observerPositionAtom.onMount = (set) => {
-  const watchId = window.navigator.geolocation.watchPosition((newPosition) => {
-    set({
-      latitude: newPosition.coords.latitude,
-      longitude: newPosition.coords.longitude,
-      height:
-        newPosition.coords.altitude !== null ? newPosition.coords.altitude : 0,
-    });
-  });
+export const geolocationAtom = atom<
+  GeolocationPosition | GeolocationPositionError | null
+>(null);
+
+geolocationAtom.onMount = (set) => {
+  const watchId = window.navigator.geolocation.watchPosition(
+    (position) => {
+      set(position);
+    },
+    (error) => {
+      set(error);
+    },
+    {
+      enableHighAccuracy: true,
+    }
+  );
 
   return () => {
     window.navigator.geolocation.clearWatch(watchId);
   };
 };
 
+interface ObserverPosition {
+  latitude: number;
+  longitude: number;
+  height: number;
+}
+
+export const observerPositionAtom = atom(
+  (get): ObserverPosition => {
+    const mode = get(observerPositionModeAtom);
+
+    if (mode === "currentPosition") {
+      const position = get(geolocationAtom);
+      if (position && "coords" in position) {
+        return {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          height: position.coords.altitude
+            ? position.coords.altitude / 1000
+            : 0,
+        };
+      }
+    }
+
+    return get(observerPositionSavedAtom);
+  },
+  (get, set, update: (prev: ObserverPosition) => ObserverPosition) => {
+    set(observerPositionSavedAtom, update(get(observerPositionAtom)));
+    set(observerPositionModeAtom, "manual");
+  }
+);
+
 export const observerGdAtom = atom<satellite.GeodeticLocation>((get) => {
-  const { latitude, longitude, height } = get(observerPositionAtom);
+  const x = get(observerPositionAtom);
   return {
-    latitude: degToRad(latitude),
-    longitude: degToRad(longitude),
-    height: height / 1000,
+    latitude: degToRad(x.latitude),
+    longitude: degToRad(x.longitude),
+    height: x.height / 1000,
   };
 });
