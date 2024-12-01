dabf0f609069164ed2693db699f0f8d42c51a972
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-11-30 19:03:12 -0600

    Put satellite labels in front of cardinal direction labels.

--- src/FirstPersonSkyView/scenePositions.ts
+++ src/FirstPersonSkyView/scenePositions.ts
@@ -1,9 +1,6 @@
 export namespace radii {
-  // 3D objects
   export const gridLine = 110;
-  export const satellitePoint = 100;
-
-  // Labels. Labels always render on top of 3D objects.
   export const gridLabel = 110;
   export const cardinalDirectionLabel = 100;
+  export const satellitePoint = 90;
 }
