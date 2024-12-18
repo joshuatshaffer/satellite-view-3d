0094a581ade5acd05170f20db0943d357522c9ba
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-12-18 10:11:05 -0600

    Find workaround for the bug in Vite 5.4.9 and later.

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,16 @@ export default defineConfig({
   ],
   base: "/satellite/",
 
+  esbuild: {
+    // Need to explicitly set target to ES2020. Otherwise, some other statements
+    // are reordered incorrectly and when `useDefineForClassFields` is true the
+    // class fields will be initialized before constructor shorthand properties
+    // are assigned.
+    //
+    // https://github.com/vitejs/vite/issues/11722#issuecomment-1831895962
+    target: "ES2020",
+  },
+
   server: {
     host: "0.0.0.0",
   },
