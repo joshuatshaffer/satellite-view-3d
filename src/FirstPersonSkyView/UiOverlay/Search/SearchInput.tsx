551cee7253f31a18cb66777c26ae16b463da05fe
Joshua Shaffer <joshua@joshuatshaffer.com>
2025-07-02 21:31:38 -0500

    Auto focus search input.

--- src/FirstPersonSkyView/UiOverlay/Search/SearchInput.tsx
+++ src/FirstPersonSkyView/UiOverlay/Search/SearchInput.tsx
@@ -1,23 +1,28 @@
 import { useAtom } from "jotai";
+import { useRef } from "react";
 import { searchTextAtom } from "../../urlAtom";
 
 export function SearchInput() {
   const [searchText, setSearchText] = useAtom(searchTextAtom);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div>
       <input
+        ref={inputRef}
         type="search"
         placeholder="Search satellites"
         value={searchText}
         onChange={(event) => {
           setSearchText(event.currentTarget.value);
         }}
+        autoFocus
       />
       <button
         type="button"
         onClick={() => {
           setSearchText("");
+          inputRef.current?.focus();
         }}
       >
         X
